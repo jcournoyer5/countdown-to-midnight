f221404b6c4005aed0201f755300f3f716f58279
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-13 21:30:55 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -4,14 +4,12 @@ def countdown(counter)
 while counter > 0
   puts "#{counter} SECOND(S)!"
   counter -= 1
-  
-end
- 
-  puts "HAPPY NEW YEAR!"
+   "HAPPY NEW YEAR!"
 # => nil (return value)
     
 end
 
 def countdown_with_sleep
   puts sleep(5.seconds)
+end
 end
