ccc9418307f82748a8410ccdfc18e1ef2b26e401
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-13 21:15:00 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,11 @@
 #write your code here
 
 def countdown(counter)
-counter = 11
+counter = 10
 while counter > 0
   counter -= 1
   puts "#{counter} SECOND(S)!"
+end
  
   puts "HAPPY NEW YEAR!"
 # => nil (return value)
