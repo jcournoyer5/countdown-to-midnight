11fc00f6a51dc5ae8a0193537382dc0f85753bc2
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-13 19:53:55 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
 def countdown
+    countdown = 10
+# => 0 (return value)
+
+while countdown < 10
+  countdown += 1
+  puts "#{number} SECOND(S)!"
+end
+# => nil (return value)
+
+puts "HAPPY NEW YEAR!"
+# => nil (return value)
+    
+end
+
+def countdown_with_sleep
 end
